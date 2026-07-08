c53c702c4d93876b64e15adeb9353ee2b877c93c
Ethan Joseph <ethancredo.ascc@outlook.com>
2026-07-08 22:21:58 +0800

    create main and header components

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import chefClaudeIcon from '../assets/img/chef-claude-icon.png'
+
+export default function Header() {
+    return (
+        <header>
+            <img src={chefClaudeIcon} alt="Chef Claude" />
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
